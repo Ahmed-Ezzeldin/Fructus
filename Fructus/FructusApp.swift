770848dba2f9e64dcba486989b1f3d20c0bc6861
Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-05-30 21:26:09 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Codemaker on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
